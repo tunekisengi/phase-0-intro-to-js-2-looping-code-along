7aa08810a9733d3ff3187ad2b35c1b1ac54d1c3b
jamesmutune <tunekivasu01@gmail.com>
2024-01-12 21:19:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let names = ['Sewer', 'Munga', 'Scar'];
+
+function writeCards(names, birthday) {
+    const messages=[];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    
+    return messages;
+}
+
+writeCards(names, "birthday");
+//while loop
+
+function countDown(num){
+    
+    while (num >=0){
+        console.log(num--);
+    }
+}
